343d6ef0fff97aa2503ce8a6e6464982b0df3de8
KarmaKoders <karmakoders@gmail.com>
2023-09-27 23:09:16 +0100

    NewUpdates

--- backend/CreateUser.js
+++ backend/CreateUser.js
@@ -1,26 +1,62 @@
-const express =require('express')
-const router =express.Router()
+const express = require('express')
+const router = express.Router()
 const User = require('./models/user.model')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const jwtSecreate = "MyJwtSecreateIs VeryUnique#@%"
 const { body, validationResult } = require('express-validator')
-router.post("/createuser",[body('email').isEmail(),body('name').isLength({min:5}),body('password',"Invalid password").isLength({min:5})],async(req,res)=>{
+router.post("/createuser", [body('email').isEmail(), body('name').isLength({ min: 5 }), body('password', "Invalid password").isLength({ min: 5 })], async (req, res) => {
     const error = validationResult(req);
-    if(!error.isEmpty()){
-        return res.status(400).json({error:error.array()})
+    if (!error.isEmpty()) {
+        return res.status(400).json({ error: error.array() })
     }
-    try{
-      await  User.create({
-            name:req.body.name,
-            password:req.body.password,
-            location:req.body.location,
-            email:req.body.email,
+    const salt = await bcrypt.genSalt(10);
+    const securePassword = await bcrypt.hash(req.body.password, salt)
+    try {
+        await User.create({
+            name: req.body.name,
+            password: securePassword,
+            location: req.body.location,
+            email: req.body.email,
 
         })
-        res.json({success:true})
+        res.json({ success: true })
 
-    }catch(err){
-console.log(err)
-res.json({success:false})
+    } catch (err) {
+        console.log(err)
+        res.json({ success: false })
     }
 })
 
-module.exports=router;
+
+
+router.post("/loginuser", [body('email').isEmail(), body('password', "Invalid password").isLength({ min: 5 })], async (req, res) => {
+
+    const email = req.body.email
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res.status(400).json({ error: error.array() })
+    }
+    try {
+        let userData = await User.findOne({ email });
+
+        if (!userData) {
+            return res.status(400).json({ error: "Try Loggning with correct Credaintles" })
+        }
+        const passwordCompair = await bcrypt.compare(req.body.password, userData.password)
+        if (req.body.password !== userData.password) {
+            return res.status(400).json({ error: "Try Loggning with correct Credaintles" })
+        }
+        const data = {
+            user: {
+                id: userData.id
+            }
+        }
+        const authToken = jwt.sign(data, jwtSecreate)
+        res.json({ success: true, authToken:authToken })
+    } catch (err) {
+        console.log(err)
+        res.json({ success: false })
+    }
+})
+module.exports = router;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,49 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 export default function Login() {
+const [credentials,setCredentials]=useState({email:"",password:""})
+const navigate =useNavigate()
+  const signUpApi = async (e) => {
+    e.preventDefault();
+       const response = await fetch("http://localhost:5000/api/loginuser",{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify({password:credentials.password,email:credentials.email,})
+       });
+       const json = await response.json();
+       console.log(json,"json---=-=-=")
+       if(!json.success){
+        alert("Enter Valid Credintails")
+       }
+       if(json.success){
+        localStorage.setItem("authToken",json.authToken);
+        navigate("/")
+       }
+            }
+const onChange=(event)=>{
+  setCredentials({...credentials,[event.target.name]:event.target.value})
+}
   return (
-    <div>Login</div>
+    <>
+    <div className='container'>
+
+    <form onSubmit={signUpApi}>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange}/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange}/>
+  </div>
+  <button type="submit" className="m-3 btn btn-primary">Submit</button>
+  <Link to="/createuser" className="m-3 btn btn-danger">Don't have a account</Link>
+</form>
+    </div>
+
+    </>
   )
 }
